fb804346d74bc65962009e40028b16ed09af9a85
yp583 <pately583@gmail.com>
2023-11-19 03:00:04 -0500

    finished cosine similarity code, pulling embeddings from MongoDB

--- articlesimilarity/mongodbpull.py
+++ articlesimilarity/mongodbpull.py
@@ -6,7 +6,7 @@ mongo_uri = "mongodb+srv://var123un:123@cluster1.bctyasc.mongodb.net/Database1"
 collection_name = "Collection1"
 
 
-def get_other_articles(id):
+def get_other_articles_embeddings(id):
     # Establish a connection to MongoDB
     client = pymongo.MongoClient(mongo_uri)
 
@@ -17,6 +17,31 @@ def get_other_articles(id):
 
     # Define your query (replace with your own criteria)
     query = {"_id": {"$ne": id}}
+    # Fetch data from the collection based on the query
+    result = collection.find(query)
+
+    embeddings = []
+    # Iterate over the results
+    for document in result:
+        embeddings.append(document["embeddings"])
+
+
+    # Close the MongoDB connection
+    client.close()
+
+    return embeddings
+
+def get_this_articles_embeddings(id):
+    # Establish a connection to MongoDB
+    client = pymongo.MongoClient(mongo_uri)
+
+    # Access the database and collection
+    db = client.get_database()
+    collection = db[collection_name]
+
+
+    # Define your query (replace with your own criteria)
+    query = {"_id": id}
     # Fetch data from the collection based on the query
     result = collection.find(query)
 

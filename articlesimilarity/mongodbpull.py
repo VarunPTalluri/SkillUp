a85599999ad7fa4c695e6e9c059dcb7ae8da0dce
yp583 <pately583@gmail.com>
2023-11-19 02:48:39 -0500

    created code to query embeddings from MongoDB

--- articlesimilarity/mongodbpull.py
+++ articlesimilarity/mongodbpull.py
@@ -1,27 +1,34 @@
 import pymongo
+import numpy as np
 
-mongo_uri = 'mongodb+srv://var123un:123@cluster1.bctyasc.mongodb.net/?retryWrites=true&w=majority'
+# Replace these values with your MongoDB connection details
+mongo_uri = "mongodb+srv://var123un:123@cluster1.bctyasc.mongodb.net/Database1"
 collection_name = "Collection1"
 
-# Establish a connection to MongoDB
-client = pymongo.MongoClient(mongo_uri)
 
-# Access the database and collection
-db = client["Database1"]
-collection = db[collection_name]
-result = collection.find({})
+def get_other_articles(id):
+    # Establish a connection to MongoDB
+    client = pymongo.MongoClient(mongo_uri)
 
-for document in result:
-    print(document)  # Process or access each document as needed
+    # Access the database and collection
+    db = client.get_database()
+    collection = db[collection_name]
 
-client.close()
 
-# Query to pull data (replace with your own query)
-"""def get_other_articles(id):
+    # Define your query (replace with your own criteria)
+    query = {"_id": {"$ne": id}}
+    # Fetch data from the collection based on the query
+    result = collection.find(query)
 
-    # Fetch data from the collection
-    result = collection.find({})
+    embeddings = []
+    # Iterate over the results
+    for document in result:
+        embeddings.append(document["embeddings"])
 
-    documents_list = list(result)
-    print(documents_list)  
-get_other_articles(0)"""
+
+    # Close the MongoDB connection
+    client.close()
+
+    return embeddings
+
+#get_other_articles(1)
